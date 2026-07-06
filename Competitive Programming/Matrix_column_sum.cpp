c7051cf2d079af4ae1e0a32458cc8271a59696af
ashraf452b <ashraf452b@gmail.com>
2026-07-06 20:11:47 +0600

    problem added

---
+++ Competitive Programming/Matrix_column_sum.cpp
@@ -0,0 +1,39 @@
+/**
+*   In the name of Allah, the Most Gracious, the Most Merciful.
+*   Author : Ashraful Islam
+*   Time & Date : 20:09:41 06/07/2026
+**/
+#include <bits/stdc++.h>
+using namespace std;
+#define MESSI ios::sync_with_stdio(false); cin.tie(0);
+#define ll long long
+#define pb push_back
+#define int long long
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define lcm(a, b) ((a) * (b)) / __gcd((a), (b))
+#define gcd(a, b) __gcd((a), (b))
+#define nl '\n'
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+void rhafsolve()
+{
+    int n,m;    cin>>n>>m;
+    vector<vector<int>> v;
+}
+//observation
+/**
+ 
+ **/
+signed main() {
+    MESSI;
+
+    int t = 1;
+    cin >> t;
+    while (t--) 
+    {
+        rhafsolve();
+    }
+
+    return 0;
+}
