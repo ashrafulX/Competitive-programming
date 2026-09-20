3ba517d6de7d5b49ad87a0b8b40c0ceb26cf1701
Ashrafulx <ashrafulwho@gmail.com>
2026-09-20 18:56:37 +0600

    problem solved

---
+++ Competitive Programming/Ardent_Butterfly_Collector.cpp
@@ -0,0 +1,50 @@
+/**
+*   In the name of Allah, the Most Gracious, the Most Merciful.
+*   Author : Ashraful Islam
+*   Time & Date : 03:16:57 20/09/2026
+**/
+#include <bits/stdc++.h>
+using namespace std;
+#define MESSI ios::sync_with_stdio(false); cin.tie(0);
+#define ll long long
+#define pb push_back
+#define int long long
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define lcm(a, b) ((a) * (b)) / __gcd((a), (b))
+#define gcd(a, b) __gcd((a), (b))
+#define nl '\n'
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+void rhafsolve()
+{
+    int n;  cin>>n;
+    set<int> st;
+    while(n--)
+    {
+        int a;  cin>>a;
+        st.insert(a);
+    }
+    int t;  cin>>t;
+    while(t--)
+    {
+        int val;    cin>>val;
+        if(st.count(val)) yes; else no;
+    }
+}
+//observation
+/**
+ 
+ **/
+signed main() {
+    MESSI;
+
+    int t = 1;
+    // cin >> t;
+    while (t--) 
+    {
+        rhafsolve();
+    }
+
+    return 0;
+}
---
+++ Competitive Programming/Different_different.cpp
@@ -0,0 +1,45 @@
+/**
+*   In the name of Allah, the Most Gracious, the Most Merciful.
+*   Author : Ashraful Islam
+*   Time & Date : 03:13:29 20/09/2026
+**/
+#include <bits/stdc++.h>
+using namespace std;
+#define MESSI ios::sync_with_stdio(false); cin.tie(0);
+#define ll long long
+#define pb push_back
+#define int long long
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define lcm(a, b) ((a) * (b)) / __gcd((a), (b))
+#define gcd(a, b) __gcd((a), (b))
+#define nl '\n'
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+void rhafsolve()
+{
+    int n;  cin>>n;
+    set<int> st;
+    while(n--)
+    {
+        int a;  cin>>a;
+        st.insert(a);
+    }
+    cout<<st.size()<<nl;
+}
+//observation
+/**
+ 
+ **/
+signed main() {
+    MESSI;
+
+    int t = 1;
+    // cin >> t;
+    while (t--) 
+    {
+        rhafsolve();
+    }
+
+    return 0;
+}
